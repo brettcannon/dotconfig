5761a55071a64e619647d0189d8f5111e4478fdf
Brett Cannon <brett@python.org>
2019-07-20 13:54:42 -0700

    Properly set git prompt settings in fish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,11 +1,11 @@
 function fish_prompt
-    set __fish_git_prompt_color_branch yellow
-    set __fish_git_prompt_showdirtystate 'yes'
-    set __fish_git_prompt_showuntrackedfiles 'yes'
-    #set __fish_git_prompt_char_dirtystate '⚡'
-    #set __fish_git_prompt_char_stagedstate '→'
-    #set __fish_git_prompt_char_untrackedfiles '☡'
-    #set __fish_git_prompt_char_stashstate '↩'
+    set -g -x __fish_git_prompt_color_branch yellow
+    set -g -x __fish_git_prompt_showdirtystate 'yes'
+    set -g -x __fish_git_prompt_showuntrackedfiles 'yes'
+    set -g -x __fish_git_prompt_char_dirtystate '⚡'
+    set -g -x __fish_git_prompt_char_stagedstate '→'
+    set -g -x __fish_git_prompt_char_untrackedfiles '☡'
+    set -g -x __fish_git_prompt_char_stashstate '↩'
     #set __fish_git_prompt_char_upstream_ahead '+'
     #set __fish_git_prompt_char_upstream_behind '-'
     set_color normal
