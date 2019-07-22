f503bce6f136975e524b374fc82a65882677ebdc
Brett Cannon <brett@python.org>
2019-07-21 17:22:33 -0700

    Update characters used for git state

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,11 +1,14 @@
 function fish_prompt
-    set -g -x __fish_git_prompt_color_branch yellow
-    set -g -x __fish_git_prompt_showdirtystate 'yes'
-    set -g -x __fish_git_prompt_showuntrackedfiles 'yes'
-    set -g -x __fish_git_prompt_char_dirtystate '⚡'
-    set -g -x __fish_git_prompt_char_stagedstate '→'
-    set -g -x __fish_git_prompt_char_untrackedfiles '☡'
-    set -g -x __fish_git_prompt_char_stashstate '↩'
+    set -g __fish_git_prompt_showcolorhints 'yes'
+    set -g __fish_git_prompt_color_branch yellow
+    set -g __fish_git_prompt_showdirtystate 'yes'
+    set -g __fish_git_prompt_char_dirtystate '🧩'
+    set -g __fish_git_prompt_char_stagedstate '→'
+    set -g __fish_git_prompt_char_invalidstate '✖'
+    set -g __fish_git_prompt_showuntrackedfiles 'yes'
+    set -g __fish_git_prompt_char_untrackedfiles '🆕'
+    set -g __fish_git_prompt_showstashstate 'yes'
+    set -g __fish_git_prompt_char_stashstate '↩'
     #set __fish_git_prompt_char_upstream_ahead '+'
     #set __fish_git_prompt_char_upstream_behind '-'
     set_color normal
