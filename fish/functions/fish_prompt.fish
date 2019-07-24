743103f3154db772902877c7e48d6df99fa5d6bf
Brett Cannon <brett@python.org>
2019-07-24 15:06:42 -0700

    Don't worry about git stash state

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,7 +3,6 @@ function fish_prompt
     set -g __fish_git_prompt_color_branch yellow
     set -g __fish_git_prompt_showdirtystate 'yes'
     set -g __fish_git_prompt_showuntrackedfiles 'yes'
-    set -g __fish_git_prompt_showstashstate 'yes'
 
     set_color normal
     if __fish_git_prompt
